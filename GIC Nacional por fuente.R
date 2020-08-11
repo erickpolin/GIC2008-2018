d11dd71b87a332727f3504f38be16c5df5284cb0
erickpolin <erick.polin@gmail.com>
2020-08-11 13:06:40 +0200

    los cambios d ela vida

--- GIC Nacional por fuente.R
+++ GIC Nacional por fuente.R
@@ -20,3 +20,70 @@ Deciles_por_fuente_2018<-read.dbf("C:/Users/Erick/Dropbox/GIC/GITHUB2018/GIC/ENI
 
 names(Deciles_por_fuente_2018)=c("ING COR2018", "TRABAJO2018", "SUBORDINADO2018", "NEGOCIOS2018","OTROS TRAB2018", "RENTAS2018","UTILIDAD2018", "ARRENDA2018", "TRANSFER2018","JUBILACION2018", "BECAS2018", "DONATIVOS2018", "REMESAS2018", "BENEGOBIERNO2018", "TRANS HOG2018", "TRANS INST2018", "ESTIM ALQU2018", "OTROS INGRESOS2018")
 
+Deciles_por_fuente_2018$TRABAJO2018+Deciles_por_fuente_2018$RENTAS2018+
+  Deciles_por_fuente_2018$JUBILACION2018+Deciles_por_fuente_2018$BECAS2018+
+  Deciles_por_fuente_2018$DONATIVOS2018+Deciles_por_fuente_2018$REMESAS2018+
+  Deciles_por_fuente_2018$BENEGOBIERNO2018+Deciles_por_fuente_2018$`TRANS HOG2018`+
+  Deciles_por_fuente_2018$`TRANS INST2018`+Deciles_por_fuente_2018$`ESTIM ALQU2018`+
+  Deciles_por_fuente_2018$`OTROS INGRESOS2018`
+
+Tasa_total<-((Deciles_por_fuente_2018$`ING COR2018`- Deciles_por_fuente_2008$`ING COR2008`)/Deciles_por_fuente_2008$`ING COR2008`)*100
+media_total<-Tasa_total[1]
+Tasa_total<-Tasa_total[-1]
+
+
+Tasa_trabajo<-((Deciles_por_fuente_2018$TRABAJO2018-Deciles_por_fuente_2008$TRABAJO2008)/Deciles_por_fuente_2008$TRABAJO2008)*100
+media_trabajo<-Tasa_trabajo[1]
+Tasa_trabajo<-Tasa_trabajo[-1]  
+
+
+Tasa_rentas<-((Deciles_por_fuente_2018$RENTAS2018-Deciles_por_fuente_2008$RENTAS2008)/Deciles_por_fuente_2008$RENTAS2008)*100
+media_rentas<-Tasa_rentas[1]
+Tasa_rentas<-Tasa_rentas[-1]
+
+
+Tasa_jubilaciones<-((Deciles_por_fuente_2018$JUBILACION2018-Deciles_por_fuente_2008$JUBILACION2008)/Deciles_por_fuente_2008$JUBILACION2008)*100
+media_jubilaciones<-Tasa_jubilaciones[1]
+Tasa_jubilaciones<-Tasa_jubilaciones[-1]
+
+Tasa_becas<-((Deciles_por_fuente_2018$BECAS2018-Deciles_por_fuente_2008$BECAS2008)/ Deciles_por_fuente_2008$BECAS2008)*100
+media_becas<-Tasa_becas[1]
+Tasa_becas<-Tasa_becas[-1]  
+  
+
+Tasa_donativos<-((Deciles_por_fuente_2018$DONATIVOS2018-Deciles_por_fuente_2008$DONATIVOS2008)/Deciles_por_fuente_2008$DONATIVOS2008)*100
+media_donativos<-Tasa_donativos[1]
+Tasa_donativos<-Tasa_donativos[-1]
+
+
+Tasa_remesas<-((Deciles_por_fuente_2018$REMESAS2018-Deciles_por_fuente_2008$REMESAS2008)/Deciles_por_fuente_2008$REMESAS2008)*100
+media_remesas<-Tasa_remesas[1]
+Tasa_remesas<-Tasa_remesas[-1]
+
+
+Tasa_benegobierno<-(Deciles_por_fuente_2018$BENEGOBIERNO2018-Deciles_por_fuente_2008$BENEGOBIERNO2008)/Deciles_por_fuente_2008$BENEGOBIERNO2008
+media_benegobierno<-Tasa_benegobierno[1]
+Tasa_benegobierno<-Tasa_benegobierno[-1]
+
+
+Tasa_transdehogares<-((Deciles_por_fuente_2018$`TRANS HOG2018`-Deciles_por_fuente_2008$`TRANS HOG2008`)/Deciles_por_fuente_2008$`TRANS HOG2008`)*100
+media_transdehogares<-Tasa_transdehogares[1]
+Tasa_transdehogares<-Tasa_transdehogares[-1]
+
+
+Tasa_instituciones<-((Deciles_por_fuente_2018$`TRANS INST2018`-Deciles_por_fuente_2008$`TRANS INST2008`)/Deciles_por_fuente_2008$`TRANS INST2008`)*100
+media_instituciones<-Tasa_instituciones[1]
+Tasa_instituciones<-Tasa_instituciones[-1]
+
+
+Tasa_alquiler<-((Deciles_por_fuente_2018$`ESTIM ALQU2018`-Deciles_por_fuente_2008$`ESTIM ALQU2008`)/Deciles_por_fuente_2008$`ESTIM ALQU2008`)*100
+media_alquiler<-Tasa_alquiler[1]
+Tasa_alquiler<-Tasa_alquiler[-1]
+
+Tasa_otros<-((Deciles_por_fuente_2018$`OTROS INGRESOS2018`-Deciles_por_fuente_2008$`OTROS INGRESOS2008`)/Deciles_por_fuente_2008$`OTROS INGRESOS2008`)*100
+media_otros<-Tasa_otros[1]
+Tasa_otros<-Tasa_otros[-1]
+
+Tasas<-data.frame(Tasa_total,Tasa_trabajo,Tasa_rentas,Tasa_jubilaciones,Tasa_becas,Tasa_donativos,Tasa_remesas,Tasa_benegobierno,
+Tasa_transdehogares,Tasa_instituciones,Tasa_alquiler,Tasa_otros)
+
--- GIC Nacional total.R
+++ GIC Nacional total.R
@@ -5,8 +5,8 @@ library(htmlwidgets)
 
 setwd(c("C:/Users/Erick/Dropbox/GIC/GITHUB2018/GIC/GIC2008-2018/GIC2008-2018"))
 
-Deciles2008<-read.dbf(choose.files())
-Deciles2018<-read.dbf(choose.files())
+Deciles2008<-read.dbf("C:/Users/Erick/Dropbox/GIC/GITHUB2018/GIC/ENIGH2008/Nacional/Nacional ingresos por decil y GINI 2008.dbf")
+Deciles2018<-read.dbf("C:/Users/Erick/Dropbox/GIC/GITHUB2018/GIC/ENIGH 2018/ENIGH2018/Nacional/Nacional ingresos por decil y GINI .dbf")
 
 GICNacionalTotal<-data.frame(Deciles2008,Deciles2018)
 
@@ -14,24 +14,36 @@ names(GICNacionalTotal)[1]<-c("Year2008")
 
 names(GICNacionalTotal)[2]<-c("Year2018")
 
+
 GICNacionalTotal<-GICNacionalTotal[-c(1),]
 
-#
+
 GICNacionalTotal<-GICNacionalTotal%>%
   mutate(Rate=((Year2018-Year2008)/Year2008)*100,Deciles=c("I","II","III","IV","V","VI","VII","VIII","IX","X"),
          orden=1:10)
 
-theme_update(plot.title = element_text(hjust = 0.5))
+media<-mean(GICNacionalTotal$Rate)
+mediana<-median(GICNacionalTotal$Rate)
+
+mytheme<-theme(plot.title = element_text(family = "Helvetica", face = "bold", size = (15),hjust = 0.5), 
+               legend.title = element_text(colour = "steelblue",  face = "bold.italic", family = "Helvetica"), 
+               legend.text = element_text(face = "italic", colour="steelblue4",family = "Helvetica"), 
+               axis.title = element_text(family = "Helvetica", size = (10), colour = "steelblue4"),
+               axis.text = element_text(family = "Courier", colour = "cornflowerblue", size = (10)))
+
+
 
 GIC_Nacional_total<-GICNacionalTotal%>%
   mutate(Deciles=fct_relevel(Deciles,"I","II","III","IV","V","VI","VII","VIII","IX","X"))%>%
   ggplot(aes(Deciles,Rate))+
   geom_col()+
-  ggtitle("Growth Incidence Curve
-          Mexico (2008-2018)")+
-  theme(plot.title = element_text(hjust = 0.5))+
-  ylab("Rate growth (total)")+
-  xlab("Decile")+
+  labs(title = "Growth Incidence Curve Mexico 2008-2018",
+                    y="Growth rate (total)",
+                    x="Decile")+
+  geom_abline(intercept = media,slope=0,col="red")+
+  annotate("text", label = "Mean = -2.34", x = 1, y = media-0.7, size = 4, colour = "red")+
+  geom_abline(intercept = mediana,slope=0,col="blue")+
+  annotate("text", label = "Median = -1.07", x = 1, y = mediana+0.5, size = 4, colour = "blue")+
   theme_minimal()
 
 GIC_Nacional_total
